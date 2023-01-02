1799661ba73a30c86a992d415076b4210d7632ef
Mykyta Prykhodchenko <nikitaprikhodchenko1@gmail.com>
2023-01-03 00:54:19 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -14,8 +14,8 @@ const Map = ({ searchResults }: Props) => {
 	const [selectedLocation, setSelectedLocation] = useState({});
 
 	const coordinates = searchResults?.map((result) => ({
-		longitude: result.long,
-		latitude: result.lat,
+		longitude: result.long?,
+		latitude: result.lat?,
 	}));
 
 	const center = getCenter(coordinates);
@@ -23,8 +23,8 @@ const Map = ({ searchResults }: Props) => {
 	const [viewport, setViewport] = useState({
 		width: '100%',
 		height: '100%',
-		latitude: center.latitude,
-		longitude: center.longitude,
+		latitude: center.latitude?,
+		longitude: center.longitude?,
 		zoom: 11,
 	});
 
