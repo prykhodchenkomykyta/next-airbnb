0529f85602a1e59ed489bc2694aaa94d62a1df80
Mykyta Prykhodchenko <nikitaprikhodchenko1@gmail.com>
2023-01-03 01:26:08 +0100

    Update Map.tsx

--- components/Map.tsx
+++ components/Map.tsx
@@ -15,8 +15,8 @@ const Map = ({ searchResults }: Props) => {
 	const [selectedLocation, setSelectedLocation] = useState({});
 
 	const coordinates = searchResults?.map((result) => ({
-		longitude: result.long,
-		latitude: result.lat,
+		longitude: result?.long,
+		latitude: result?.lat,
 	}));
 
 	const center = getCenter(coordinates);
